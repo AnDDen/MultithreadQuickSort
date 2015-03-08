72e10607b68596657de77b54a9c8a2431aaf8a46
AnDDen <androwd@gmail.com>
2015-03-08 22:47:18 +0300

    more examples

---
+++ MultithreadQuickSort/MultithreadQSort.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MultithreadQuickSort
+{
+    public class MultithreadQSort
+    {
+        public static bool log = true;
+
+        private static Semaphore pool;
+        private static int t = 0, count = 0;
+
+        public static void QuickSort(int[] a)
+        {
+            pool = new Semaphore(8, 16);
+
+            count = 1;
+            ThreadPool.QueueUserWorkItem(new WaitCallback(QuickSort), new object[] { a, 0, a.Length - 1 });
+
+            while (count != 0) ;
+
+            if (log) Console.WriteLine("Sort complete");
+
+        }
+
+        private static void QuickSort(object state)
+        {
+            int num = t++;
+            if (log) Console.WriteLine("[Thread {0}]\tEnters thread pool and waits for the semaphore.", Thread.CurrentThread.ManagedThreadId);
+
+            pool.WaitOne();
+
+            if (log) Console.WriteLine("[Thread {0}]\tEnters the semaphore.", Thread.CurrentThread.ManagedThreadId);
+
+            //extracting arguments
+            object[] array = state as object[];
+            int[] a = array[0] as int[];
+            int p = Convert.ToInt32(array[1]);
+            int r = Convert.ToInt32(array[2]);
+
+            if (p < r)
+            {
+                int q = Partition(a, p, r);
+                count += 2;
+                ThreadPool.QueueUserWorkItem(new WaitCallback(QuickSort), new object[] { a, p, q - 1 });
+                ThreadPool.QueueUserWorkItem(new WaitCallback(QuickSort), new object[] { a, q + 1, r });
+            }
+
+            int k = pool.Release();
+            if (log) Console.WriteLine("[Thread {0}]\tReleases the semaphore. Previous semaphore count: {1}", Thread.CurrentThread.ManagedThreadId, k);
+
+            count--;
+        }
+
+        private static int Partition(int[] a, int p, int r)
+        {
+            int t;
+            int i = p - 1;
+            int x = a[r];
+
+            for (int j = p; j < r; j++)
+            {
+                if (a[j] <= x)
+                {
+                    t = a[++i];
+                    a[i] = a[j];
+                    a[j] = t;
+                }
+            }
+
+            t = a[i + 1];
+            a[i + 1] = a[r];
+            a[r] = t;
+            return i + 1;
+        }
+    }
+}
--- MultithreadQuickSort/Program.cs
+++ MultithreadQuickSort/Program.cs
@@ -11,12 +11,66 @@ namespace MultithreadQuickSort
     class Program
     {
         //private static StreamWriter input = new StreamWriter("input.txt");
-        //private static StreamWriter output = new StreamWriter("output.txt");
+        private static StreamWriter output = new StreamWriter("output.txt");
 
         static void Main(string[] args)
         {
-            //int[] array = new int[] { 10, 8, 23, 24, 35, 34, 24, 44, 25, 5, 6, -7, 15, 35, 23, 45, 35, 3, 446, 45, 49, 42, 65, 132, 34 };
+            MultithreadQSort.log = false;
 
+            /* int[] array = new int[] { 10, 8, 23, 24, 35, 34, 24, 44, 25, 5, 6, -7, 15, 35, 23, 45, 35, 3, 
+                446, 45, 49, 42, 65, 132, 34, 35, 325, 67, 33, 54, 23, 4, -21, -42, -34234, -42, -243 };
+
+            int[] array1 = new int[] { 10, 8, 23, 24, 35, 34, 24, 44, 25, 5, 6, -7, 15, 35, 23, 45, 35 }; */
+
+            /* Array.ForEach(array, (x) => { Console.Write("{0} ", x); });
+            Console.WriteLine();
+            Array.ForEach(array1, (x) => { Console.Write("{0} ", x); });
+            Console.WriteLine(); */
+
+            Random rnd = new Random();
+
+            List<int[]> arrays = new List<int[]>();
+            List<int[]> arraysB = new List<int[]>();
+
+
+            for (int j = 0; j < 10; j++)
+            {
+                arrays.Add(new int[500000]);
+                arraysB.Add(new int[500000]);
+
+                for (int i = 0; i < arrays[j].Length; i++)
+                {
+                    arrays[j][i] = rnd.Next(1000000);
+                    arraysB[j][i] = arrays[j][i];
+                }
+            }
+
+            Console.WriteLine("Multithread Sorts start");
+
+            DateTime start = DateTime.Now;
+
+            for (int j = 0; j < 10; j++)
+                MultithreadQSort.QuickSort(arrays[j]);
+
+            DateTime finish = DateTime.Now;
+            Console.WriteLine("Multithread Sorts finish. Time: {0} s", (finish - start).TotalSeconds);
+
+            for (int j = 0; j < 10; j++)
+            {
+                output.WriteLine("Array {0}", j);
+                Array.ForEach(arrays[j], (x) => { output.Write("{0} ", x); });
+                output.WriteLine();
+                output.WriteLine();
+            }
+
+            Console.WriteLine("Not-multithread Sorts start");
+            start = DateTime.Now;
+            for (int j = 0; j < 10; j++)
+                QSort(arraysB[j]);
+            finish = DateTime.Now;
+            Console.WriteLine("Multithread Sorts finish. Time: {0} s", (finish - start).TotalSeconds); 
+
+            /*
             Random rnd = new Random();
 
             for (int j = 0; j < 10; j++)
@@ -31,7 +85,7 @@ namespace MultithreadQuickSort
 
                 Console.WriteLine("[Array {0}] multithread sort start", j + 1);
                 DateTime start = DateTime.Now;
-                QuickSort(array);
+                MultithreadQSort.QuickSort(array);
                 DateTime finish = DateTime.Now;
                 Console.WriteLine("[Array {0}] multithread sort finish", j + 1);
 
@@ -45,62 +99,30 @@ namespace MultithreadQuickSort
 
                 Console.WriteLine("[Array {0}] non-multithread sort time: {1} s", j + 1, (finish - start).TotalSeconds);
                 Console.WriteLine();
-            }
+            } 
 
-            //output.Flush();
-            //output.Close();
+            */
+
+            output.Flush();
+            output.Close();
 
             Console.ReadKey();
         }
 
-        private static Semaphore pool;
-        private static int t = 0, count = 0;
-
-        private static bool log = false;
-
-        public static void QuickSort(int[] a)
+        // сортировка без потоков
+        public static void QSort(int[] a)
         {
-            pool = new Semaphore(5, 5);
-
-            count = 1;
-            ThreadPool.QueueUserWorkItem(new WaitCallback(QuickSort), new object[] { a, 0, a.Length - 1 });
-
-            while (count != 0) ;
-
-            //if (log) Console.WriteLine("Sort complete");
-
+            qsort(a, 0, a.Length - 1);
         }
 
-        private static void QuickSort(object state)
+        private static void qsort(int[] a, int p, int r)
         {
-            //int num = t++;
-            //if (log) Console.WriteLine("[Thread {0}]\tEnters thread pool and waits for the semaphore.", num);
-
-            pool.WaitOne();
-
-            //if (log) Console.WriteLine("[Thread {0}]\tEnters the semaphore.", num);
-       
-            //extracting arguments
-            object[] array = state as object[];
-            int[] a = array[0] as int[];
-            int p = Convert.ToInt32(array[1]);
-            int r = Convert.ToInt32(array[2]);
-
-            //Console.WriteLine("[Thread {0}]\tp = {1}; r = {2}", num, p, r);
-
             if (p < r)
             {
                 int q = Partition(a, p, r);
-                //Console.WriteLine("[Thread {0}]\tq = {1}", num, q);
-                count += 2;
-                ThreadPool.QueueUserWorkItem(new WaitCallback(QuickSort), new object[] { a, p, q - 1 });
-                ThreadPool.QueueUserWorkItem(new WaitCallback(QuickSort), new object[] { a, q + 1, r });
+                qsort(a, p, q - 1);
+                qsort(a, q + 1, r);
             }
-
-            int k = pool.Release();
-            //if (log) Console.WriteLine("[Thread {0}]\tReleases the semaphore. Previous semaphore count: {1}", num, k);
-
-            count--;
         }
 
         private static int Partition(int[] a, int p, int r)
@@ -123,22 +145,6 @@ namespace MultithreadQuickSort
             a[i + 1] = a[r];
             a[r] = t;
             return i + 1;
-        }
-
-        // сортировка без потоков
-        public static void QSort(int[] a)
-        {
-            qsort(a, 0, a.Length - 1);
-        }
-
-        private static void qsort(int[] a, int p, int r)
-        {
-            if (p < r)
-            {
-                int q = Partition(a, p, r);
-                qsort(a, p, q - 1);
-                qsort(a, q + 1, r);
-            }
         }
     }
 }
